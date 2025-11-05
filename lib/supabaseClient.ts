d4a0526445f97080a00c04771bb29a16763b4e58
Esteban Gamboa <81179481+estebanGamboa-12@users.noreply.github.com>
2025-11-05 18:34:18 +0100

    Fix server Supabase client syntax

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,22 +1,9 @@
-import { createServerClient } from "@supabase/auth-helpers-nextjs";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
 import type { Database, TypedSupabaseClient } from "@/lib/database.types";
 
 export const createSupabaseServerClient = (): TypedSupabaseClient =>
   createServerComponentClient<Database>({ cookies });
-
-  return createServerClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
-        }
-      }
-    }
-  );
-};
 
 export type { TypedSupabaseClient };
